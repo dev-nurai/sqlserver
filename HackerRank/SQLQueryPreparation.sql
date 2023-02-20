1f908baa9211fe435e1f7c5daad3a2734134f5a3
nurai <coder.nurai@gmail.com>
2023-02-20 16:29:27 +0530

    New problems solved on HackerRank

--- HackerRank/SQLQueryPreparation.sql
+++ HackerRank/SQLQueryPreparation.sql
@@ -99,3 +99,40 @@ SELECT Top 1 CAST(ROUND((LAT_N),4) as decimal(18,4)) FROM STATION
 WHERE LAT_N > 38.7780
 ORDER BY LAT_N;
 
+---------
+/*
+Query the total population of all cities in CITY where District is California.
+*/
+SELECT SUM(POPULATION) FROM CITY
+WHERE DISTRICT = 'California';
+
+/*
+Query the average population of all cities in CITY where District is California.
+*/
+
+SELECT AVG(POPULATION) FROM CITY
+WHERE DISTRICT = 'California';
+
+/*
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+*/
+
+SELECT AVG(POPULATION) FROM CITY;
+
+/*
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+*/
+
+SELECT SUM(POPULATION) FROM CITY
+WHERE COUNTRYCODE = 'JPN';
+
+
+/*
+Query the difference between the maximum and minimum populations in CITY.
+*/
+
+SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY
+
+/*
+
+*/
