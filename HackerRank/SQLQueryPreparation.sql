7752bc50971c9ec750ab9555e898959ed60e604a
nurai <coder.nurai@gmail.com>
2023-02-21 18:40:36 +0530

    HackerRank sql solution

--- HackerRank/SQLQueryPreparation.sql
+++ HackerRank/SQLQueryPreparation.sql
@@ -95,6 +95,7 @@ Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 3
 Round your answer to  decimal places.
 */
 
+<<<<<<< Updated upstream
 SELECT Top 1 CAST(ROUND((LAT_N),4) as decimal(18,4)) FROM STATION
 WHERE LAT_N > 38.7780
 ORDER BY LAT_N;
@@ -136,3 +137,37 @@ SELECT MAX(POPULATION) - MIN(POPULATION) FROM CITY
 /*
 
 */
+=======
+SELECT CAST(CEILING(AVG(cast(Salary as Decimal(18,2))) - 
+AVG(CAST(REPLACE(Salary, 0, '')AS Decimal(18,2)))) AS INT)
+FROM EMPLOYEES;
+
+/*
+Query the following two values from the STATION table:
+
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+
+*/
+
+SELECT CAST(SUM(LAT_N) as DECIMAL(18,2)), CAST(SUM(LONG_W) as DECIMAL(18,2))
+FROM STATION;
+
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  38.7880
+and less than 137.2345. Truncate your answer to  decimal places.
+*/
+
+SELECT CAST(SUM(LAT_N) as DECIMAL(18,4))
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
+
+/*
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. 
+Truncate your answer to  decimal places.
+*/
+SELECT TOP 1 CAST((LAT_N) as Decimal(18, 4))
+FROM STATION
+WHERE LAT_N < 137.2345
+ORDER BY LAT_N desc;
+>>>>>>> Stashed changes
